8e7350a3f177d713046482e412f4e5f2f0b41eb7
Thorgalix <lucaspodevin@sfr.fr>
2026-04-13 10:55:57 +0200

    Ajout d'un module pour sommer une liste de flottants avec annotations de type

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module for summing a list of floats with type annotations.
+
+This module provides a function that computes the sum of a list of
+floating-point numbers and returns the result as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Return the sum of a list of floats.
+
+    Args:
+        input_list: A list of floating-point numbers.
+
+    Returns:
+        The sum of all values in input_list as a float.
+    """
+    return float(sum(input_list))
