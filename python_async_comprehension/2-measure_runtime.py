552cabdae721b5f48caffe64262f86ec31dcd5a3
Thorgalix <lucaspodevin@sfr.fr>
2026-04-14 10:20:24 +0200

    Correction de l'indentation et simplification du retour dans la fonction measure_runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,11 +9,11 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Return the time needed to run four async comprehensions concurrently."""
     start = perf_counter()
-    results = await asyncio.gather(
+    await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
-        )
+    )
     end = perf_counter()
-    return (end - start)
+    return end - start
