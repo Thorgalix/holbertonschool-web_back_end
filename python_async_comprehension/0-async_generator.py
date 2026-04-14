37e25bb8c03539e5ff010b3897f22bbabd774366
Thorgalix <lucaspodevin@sfr.fr>
2026-04-14 09:40:10 +0200

    Ajout d'un paramètre de type None à l'annotation de la fonction async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import asyncio
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Yield 10 random floats between 0 and 10 with one-second intervals."""
     for i in range(10):
         await asyncio.sleep(1)
