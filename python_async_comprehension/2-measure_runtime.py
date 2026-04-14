d9ebaaff3c6557478d3ca9b1cc6b9ec8eb1d6c25
Thorgalix <lucaspodevin@sfr.fr>
2026-04-14 10:17:15 +0200

    Ajout de la fonction measure_runtime pour mesurer le temps d'exécution de plusieurs appels de compréhension asynchrone

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Measure the runtime of multiple async comprehension calls."""
+import asyncio
+from time import perf_counter
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Return the time needed to run four async comprehensions concurrently."""
+    start = perf_counter()
+    results = await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+        )
+    end = perf_counter()
+    return (end - start)
