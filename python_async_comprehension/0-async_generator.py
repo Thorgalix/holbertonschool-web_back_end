b1a5baffa22351cf87c7e69c440f5c6a8986fc9b
Thorgalix <lucaspodevin@sfr.fr>
2026-04-14 09:38:35 +0200

    Ajout de l'annotation de type pour la fonction async_generator en utilisant AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float]:
     """Yield 10 random floats between 0 and 10 with one-second intervals."""
     for i in range(10):
         await asyncio.sleep(1)
