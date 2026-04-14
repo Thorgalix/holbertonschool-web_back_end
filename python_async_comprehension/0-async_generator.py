aed1b5d7195a63eaebd66143facb141459c84ea6
Thorgalix <lucaspodevin@sfr.fr>
2026-04-14 09:46:43 +0200

    Modification de l'annotation de type de la fonction async_generator pour utiliser Generator au lieu d'AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield 10 random floats between 0 and 10 with one-second intervals."""
     for i in range(10):
         await asyncio.sleep(1)
