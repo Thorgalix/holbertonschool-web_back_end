80f8684257be22dc1e7f530e058c36bc765906b9
Thorgalix <lucaspodevin@sfr.fr>
2026-04-14 10:00:05 +0200

    Ajout de l'annotation de type pour la fonction async_comprehension en utilisant list[float]

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,6 +3,6 @@
 
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension():
+async def async_comprehension() -> list[float]:
     """Collect ten values from the async generator using an async comprehension."""
     return [x async for x in async_generator()]
