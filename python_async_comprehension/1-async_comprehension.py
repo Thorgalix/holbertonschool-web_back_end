5ed2e47c24a763eca20c0b3822644bde7ed798b5
Thorgalix <lucaspodevin@sfr.fr>
2026-04-14 09:54:16 +0200

    Ajout de la fonction async_comprehension pour collecter des valeurs à partir d'un générateur asynchrone

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Async comprehension helpers built on top of the async generator module."""
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension():
+    """Collect ten values from the async generator using an async comprehension."""
+    return [x async for x in async_generator()]
