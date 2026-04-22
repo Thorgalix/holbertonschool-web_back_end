c40ec5223007432b344f55ace17c4bd54088e472
Thorgalix <lucaspodevin@sfr.fr>
2026-04-22 11:17:30 +0200

    Suppression de code commenté pour clarifier le module de statistiques de logs

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,27 +5,6 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://127.0.0.1:27017')
 collection = client.logs.nginx
 
-"""total = collection.count_documents({})
-get = collection.count_documents({"method": "GET"})
-post = collection.count_documents({"method": "POST"})
-put = collection.count_documents({"method": "PUT"})
-patch = collection.count_documents({"method": "PATCH"})
-delete = collection.count_documents({"method": "DELETE"})
-status = collection.count_documents({
-    "method": "GET",
-    "path": "/status"
-})
-
-print(f'{total} logs')
-print('Methods:')
-print(f'\t method GET: {get}')
-print(f'\t method POST: {post}')
-print(f'\t method PUT: {put}')
-print(f'\t method PATCH: {patch}')
-print(f'\t method DELETE: {delete}')
-print(f'{status} status check')
-"""
-
 print(f'{collection.count_documents({})} logs')
 print('Methods:')
 
