ee60c2b355b41beb4e5bd679e91eb530021b6198
Thorgalix <lucaspodevin@sfr.fr>
2026-04-22 11:19:44 +0200

    Refactor le module de statistiques de logs pour encapsuler la logique dans la fonction main()

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,21 +2,28 @@
 """Module pour récupérer et afficher des stats venant de logs"""
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-collection = client.logs.nginx
 
-print(f'{collection.count_documents({})} logs')
-print('Methods:')
+def main():
+    """Affiche des statistiques sur la collection logs.nginx."""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f'{collection.count_documents({})} logs')
+    print('Methods:')
 
-for method in methods:
-    count = collection.count_documents({'method': method})
-    print(f'\tmethod {method}: {count}')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-status = collection.count_documents({
-    "method": "GET",
-    "path": "/status"
-})
+    for method in methods:
+        count = collection.count_documents({'method': method})
+        print(f'\tmethod {method}: {count}')
 
-print(f'{status} status check')
+    status = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+
+    print(f'{status} status check')
+
+
+if __name__ == "__main__":
+    main()
