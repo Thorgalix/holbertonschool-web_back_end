ed5b825261da44414856018735c29684c036246d
Thorgalix <lucaspodevin@sfr.fr>
2026-05-11 11:25:07 +0200

    Remplacement de console.log par process.stdout.write pour une gestion améliorée de l'affichage dans 1-stdin.js.

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,13 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-let isFirstLine = true;
-
-rl.on('line', (line) => {
-  if (isFirstLine) {
-    console.log(`Your name is: ${line}`);
-    isFirstLine = false;
-  }
-});
-
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+  setTimeout(() => {
+    process.exit(0);
+  }, 100);
 });
