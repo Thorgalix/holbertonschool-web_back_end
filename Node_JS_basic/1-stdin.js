fa3cc11be1e6bc3060b9185b1bee85fb09443d4f
Thorgalix <lucaspodevin@sfr.fr>
2026-05-11 11:10:29 +0200

    Suppression de l'appel à process.stdin.pause() dans la fonction de gestion des données d'entrée.

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,6 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.stdin.pause();
 });
 
 process.stdin.on('end', () => {
