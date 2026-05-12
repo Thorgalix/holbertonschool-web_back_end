f3661edc9f10ba4a94711254632bcd03f023e34f
Thorgalix <lucaspodevin@sfr.fr>
2026-05-12 12:31:14 +0200

    Correction de l'affichage du nom et simplification de la gestion des événements d'entrée

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}`);
-
-  if (process.stdin.isTTY) {
-    process.exit();
-  }
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
 });
 
-process.stdin.on('end', () => {
+process.stdin.on('close', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-process.stdin.resume();
