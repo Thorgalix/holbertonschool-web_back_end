dfb518594fb0de039a91e7f2134d72c0e4806044
Thorgalix <lucaspodevin@sfr.fr>
2026-05-12 13:39:26 +0200

    Amélioration de la gestion des données d'entrée en extrayant le nom dans une variable avant l'affichage

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\r`);
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\r`);
 });
 
-process.stdin.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
+
+process.stdin.resume();
