fd9af89bedefc0103c9bcd05563cbfab4ac61cc8
Thorgalix <lucaspodevin@sfr.fr>
2026-05-12 14:14:37 +0200

    Modification de la gestion de l'entrée utilisateur pour utiliser 'readable' au lieu de 'data' et ajustement de l'affichage du nom

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n\r`);
-  process.stdin.pause();
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-process.stdin.resume();
